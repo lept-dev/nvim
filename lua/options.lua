3986431281e5b63d723ad7a5ba40eb66017bc840
lept-dev <lept.dev@gmail.com>
2026-04-05 13:16:59 -0700

    Add options file

---
+++ lua/options.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+
+vim.opt.expandtab = true
+
+
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+
+
+vim.opt.ruler = true
+vim.opt.colorcolumn = '120'
